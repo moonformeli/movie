d1f1868ba4494968eda3486cb3b139bb2e3971f5
moonformeli <mgyang95@gmail.com>
2017-05-08 06:57:02 +0000

    router function is being built

---
+++ src/containers/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BigText from '../components/BigText';
+
+const Posts = () => {
+    return (
+        <div>
+            <BigText>포스트</BigText>
+        </div>
+    );
+};
+
+export default Posts;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
 import App from './App';
+import Home from './containers/Home';
+import About from './containers/About';
+import Posts from './containers/Posts';
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
+      <Route path="about" component={About}/>
+      <Route path="post" component={Posts}/>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
